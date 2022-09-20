f27cf0e4ab43edfabacabb2a913a3ab787c4c76e
Joseph <joffan98@gmail.com>
2022-09-20 10:49:14 -0500

    commit tree 0a93d887c66e12f2f5e3e36f18770dc0b0f654eb

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  switch (true) {
+    case distance <= 400:
+      return "This one is on me!"
+    case distance <= 2000:
+      return "That will be twenty bucks."
+    case distance <= 2500:
+      return "I will gladly take your thirty bucks."
+    case distance > 2500:
+      return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let responce
+  city === "NYC" ? (responce = "Ok, sounds good."): (responce = "No go.")
+  return responce
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default: 
+      return "Bye."
+  }
 }
